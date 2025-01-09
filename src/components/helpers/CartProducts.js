214415983ce9f192688779e2fde6ce47eed0cf2d
rowanhere <lamsalgokul917@gmail.com>
2025-01-09 14:46:04 +0545

    Extra product info and swipe code

--- src/components/helpers/CartProducts.js
+++ src/components/helpers/CartProducts.js
@@ -2,11 +2,13 @@ import React, { useRef, useState } from "react";
 import { FaHeart, FaMinus, FaPlus } from "react-icons/fa";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { useSwipeable } from "react-swipeable";
+
 const CartProducts = () => {
   const [ItemCount, setItemCount] = useState(1);
-  const swipe = useRef(null);
-  const swipeEndTrack = useRef(null);
-  const [side, setSide] = useState(null);
+  const swipeRef = useRef(null);
+  const [swipeState, setSwipeState] = useState({ direction: null, X: 0 });
+  const SWIPE_THRESHOLD = 200; // Maximum swipe distance in pixels
+  const SNAP_DISTANCE = 70; // Distance to snap on swipe end
   const config = {
     delta: 69, // min distance(px) before a swipe starts. *See Notes*
     preventScrollOnSwipe: false, // prevents scroll during swipe (*See Details*)
@@ -16,37 +18,26 @@ const CartProducts = () => {
     swipeDuration: Infinity, // allowable duration of a swipe (ms). *See Notes*
     touchEventOptions: { passive: true }, // options for touch listeners (*See Details*)
   };
-  const handleSwap = (e) => {
-    const swipeDiv = swipe.current;
-    const swipePx = e.deltaX;
-    if (e.dir === "Left") {
-      console.log("swiping Left");
-      setSide("Left");
-      const max_swipe = -200;
-      if (swipePx <= max_swipe) return;
-      swipeDiv.style.transform = `translateX(${swipePx + "px"})`;
-      swipeEndTrack.current = "Left";
-    }
-    if (e.dir === "Right") {
-      setSide("Right");
-
-      console.log("swiping Right");
-      const max_swipe = 200;
-      if (swipePx >= max_swipe) return;
-      swipeDiv.style.transform = `translateX(${swipePx + "px"})`;
-      swipeEndTrack.current = "Right";
-    }
+  const handleSwap = ({ deltaX, dir }) => {
+    const swipeDiv = swipeRef.current;
+    const minMaxDelta = Math.max(
+      -SWIPE_THRESHOLD,
+      Math.min(SWIPE_THRESHOLD, deltaX)
+    );
+    console.log(minMaxDelta);
+    setSwipeState({ direction: dir, X: minMaxDelta });
+    swipeDiv.style.transform = `translate(${minMaxDelta}px)`;
   };
-  const handleEnd = ({ event }) => {
-    const getLastTouched = event.changedTouches[0].clientX;
-    const swipeTrack = swipeEndTrack.current;
-    console.log(getLastTouched, swipeEndTrack.current);
-
-    if (getLastTouched >= 200 && swipeTrack === "Right") {
-      swipe.current.style.transform = `translateX(70px)`;
+  const handleEnd = () => {
+    const swipeDiv = swipeRef.current;
+    const { direction, X } = swipeState;
+    if (direction === "Right" && X >= SNAP_DISTANCE) {
+      swipeDiv.style.transform = `translateX(70px)`;
+      return;
     }
-    if (getLastTouched <= 200 && swipeTrack === "Left") {
-      swipe.current.style.transform = `translateX(-70px)`;
+    if (direction === "Left" && X <= SNAP_DISTANCE) {
+      swipeDiv.style.transform = `translateX(-70px)`;
+      return;
     }
   };
   const handlers = useSwipeable({
@@ -54,16 +45,23 @@ const CartProducts = () => {
     onTouchEndOrOnMouseUp: handleEnd,
     ...config,
   });
+
   return (
     <div
       onClick={() => {
-        swipe.current.style.transform = `translateX(0px)`;
+        swipeRef.current.style.transform = `translateX(0px)`;
       }}
-      className="relative overflow-hidden"
+      className="relative overflow-x-hidden overflow-y-visible"
       {...handlers}
     >
-      <div className={`absolute flex p-4 z-[1] ${side==="Left"?"bg-red-500":""} ${side==="Right"?"bg-orange-500":""} bg-opacity-40 inset-0 rounded-xl`}>
-        {side === "Left" && (
+      <div
+        className={`absolute flex p-4 z-[1] ${
+          swipeState.direction === "Left" ? "bg-red-500" : ""
+        } ${
+          swipeState.direction === "Right" ? "bg-orange-500" : ""
+        } bg-opacity-40 inset-0 rounded-xl`}
+      >
+        {swipeState.direction === "Left" && (
           <button
             className="ml-auto  text-red-600"
             onClick={(e) => console.log("Hello")}
@@ -72,51 +70,65 @@ const CartProducts = () => {
           </button>
         )}
 
-        {side === "Right" && (
+        {swipeState.direction === "Right" && (
           <button className=" text-white" onClick={(e) => console.log("Hello")}>
             <FaHeart size={25} />
           </button>
         )}
       </div>
       <div
-        ref={swipe}
-        className={`flex relative   z-[2] transition-transform duration-500 ease-out gap-2 bg-white border  shadow-lg p-2 rounded-xl`}
+        ref={swipeRef}
+        className={`flex relative min-h-[fit-content]  z-[2] transition-transform duration-500 ease-out gap-2 bg-white border  shadow-lg p-2 rounded-xl`}
       >
         {/* image  */}
-        <div className="h-[80px] rounded-xl overflow-hidden aspect-square">
+        <div className="h-[80px]   rounded-xl overflow-hidden aspect-square">
           <img
-            className="max-w-full object-center max-h-full object-cover"
+            className="max-w-full  object-center max-h-full object-cover"
             src="https://img.drz.lazcdn.com/g/kf/S3612f631a80f476d9091051e7dc1d5d4S.jpg_720x720q80.jpg_.webp"
             alt="img"
           />
         </div>
         {/* other info */}
-        <div className="flex flex-col font-[600]">
-          <span>Women Sexy Low-waisted</span>
+        <div className="flex flex-col font-[600] grow">
+          <span>Man trousers</span>
           <span className=" text-red-400">$33.65</span>
-          <div className="self-end justify-between flex items-center gap-[0.4rem]">
-            <button
-              disabled={ItemCount <= 1}
-              onClick={() => {
-                setItemCount((count) => count - 1);
-              }}
-              className={`border-2 ${
-                ItemCount <= 1
-                  ? "text-gray-400 "
-                  : "text-orange-400 border-orange-400"
-              } transition-colors ease-out duration-300 rounded-lg w-[30px] p-1 h-[28px] flex items-center justify-center text-center`}
-            >
-              <FaMinus size={15} fontWeight={400} />
-            </button>
-            <span className="w-[3ch] text-center">{ItemCount}</span>
-            <button
-              onClick={() => {
-                setItemCount((count) => count + 1);
-              }}
-              className="border-2 text-orange-400 border-orange-400 rounded-lg w-[30px] p-1 h-[28px] flex items-center justify-center text-center"
-            >
-              <FaPlus fontWeight={400} size={15} />
-            </button>
+
+          {/* Product type */}
+
+          {/* Product amount */}
+          <div className=" justify-between flex mt-1">
+            <div className="grid grid-cols-2 gap-1 mr-1">
+              <span className="border-2 text-center border-orange-400 text-orange-500 px-3 rounded-xl">
+                White
+              </span>
+              <span className="border-2 text-center border-orange-400 text-orange-500 px-3 rounded-xl">
+                XL
+              </span>
+            </div>
+            <div className="flex items-center gap-[0.4rem]">
+              <button
+                disabled={ItemCount <= 1}
+                onClick={() => {
+                  setItemCount((count) => count - 1);
+                }}
+                className={`border-2 ${
+                  ItemCount <= 1
+                    ? "text-gray-400 "
+                    : "text-orange-400 border-orange-400"
+                } transition-colors ease-out duration-300 rounded-lg w-[30px] p-1 h-[28px] flex items-center justify-center text-center`}
+              >
+                <FaMinus size={15} fontWeight={400} />
+              </button>
+              <span className="w-[3ch] text-center">{ItemCount}</span>
+              <button
+                onClick={() => {
+                  setItemCount((count) => count + 1);
+                }}
+                className="border-2 text-orange-400 border-orange-400 rounded-lg w-[30px] p-1 h-[28px] flex items-center justify-center text-center"
+              >
+                <FaPlus fontWeight={400} size={15} />
+              </button>
+            </div>
           </div>
         </div>
       </div>
